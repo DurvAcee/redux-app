d3e69301a6173e91e8da79b4f5b0aac327941e99
Durvesh Danve <Durvesh.Danve@veritas.com>
2023-03-26 20:21:04 +0530

    Added Store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Navbar from './state/components/Navbar';
-import Shop from './state/components/Shop';
+import Navbar from './components/Navbar';
+import Shop from './components/Shop';
 
 function App() {
   return (
