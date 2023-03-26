7cabebcc951d0425e2c77aa8c5dae191582099ec
Durvesh Danve <Durvesh.Danve@veritas.com>
2023-03-26 20:33:15 +0530

    minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+    const amount = useSelector(state => state.amount)
+
   return (
    <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -19,7 +22,7 @@ const Navbar = () => {
                 </li>
             </ul>
             <div>
-                <button disabled={true} className="btn-primary"> Your Balance : 10,000</button>
+                <button disabled={true} className="btn-primary"> Your Balance : {amount}</button>
             </div>
         </div>
     </nav>
--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,7 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";                         Deprecated !!
+import { legacy_createStore as createStore } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
 
-export const store = createStore(reducers, {}, applyMiddleware (thunk))
+const applyMiddleware = require("redux").applyMiddleware;
+export const store = createStore(reducers, {}, applyMiddleware(thunk))
